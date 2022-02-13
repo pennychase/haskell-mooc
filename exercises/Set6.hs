cbb834a000c112c2f4504942cd4378a596bb2cc4
Penny Chase <mpchase@mac.com>
2022-02-13 17:21:47 -0500

    Exercises 6 and 7

--- exercises/Set6.hs
+++ exercises/Set6.hs
@@ -13,7 +13,10 @@ data Country = Finland | Switzerland | Norway
   deriving Show
 
 instance Eq Country where
-  (==) = todo
+  (==) Finland Finland = True 
+  (==) Switzerland Switzerland = True 
+  (==) Norway Norway = True 
+  (==) _ _ = False
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement an Ord instance for Country so that
@@ -22,10 +25,9 @@ instance Eq Country where
 -- Remember minimal complete definitions!
 
 instance Ord Country where
-  compare = todo -- implement me?
-  (<=) = todo -- and me?
-  min = todo -- and me?
-  max = todo -- and me?
+  (<=) Finland _ = True
+  (<=) _ Switzerland = True 
+  (<=) c1 c2 = c1 == c2
 
 ------------------------------------------------------------------------------
 -- Ex 3: Implement an Eq instance for the type Name which contains a String.
@@ -41,7 +43,7 @@ data Name = Name String
   deriving Show
 
 instance Eq Name where
-  (==) = todo
+  (==) (Name n1) (Name n2) = map toLower n1 == map toLower n2
 
 ------------------------------------------------------------------------------
 -- Ex 4: here is a list type parameterized over the type it contains.
@@ -55,7 +57,9 @@ data List a = Empty | LNode a (List a)
   deriving Show
 
 instance Eq a => Eq (List a) where
-  (==) = todo
+  (==) Empty Empty = True 
+  (==) (LNode v1 l1) (LNode v2 l2) = v1 == v2 && l1 == l2
+  (==) _ _ = False
 
 ------------------------------------------------------------------------------
 -- Ex 5: below you'll find two datatypes, Egg and Milk. Implement a
@@ -75,6 +79,23 @@ data Egg = ChickenEgg | ChocolateEgg
 data Milk = Milk Int -- amount in litres
   deriving Show
 
+class Price a where
+  price :: a -> Int
+
+instance Price Egg where
+  price ChickenEgg = 20
+  price ChocolateEgg = 30
+
+instance Price Milk where
+  price (Milk amount) = 15 * amount
+
+instance Price a => Price (Maybe a) where
+  price Nothing = 0
+  price (Just x) = price x
+
+instance Price a => Price [a] where
+
+  price ls = foldr (\x y -> price x + y) 0 ls
 
 ------------------------------------------------------------------------------
 -- Ex 6: define the necessary instances in order to be able to compute these:
@@ -94,6 +115,11 @@ data Milk = Milk Int -- amount in litres
 
 data Number = Finite Integer | Infinite
   deriving (Show,Eq)
+
+instance Ord Number where 
+  (Finite n1) <= (Finite n2) = n1 <= n2
+  _ <= Infinite = True 
+  _ <= _ = False
 
 
 ------------------------------------------------------------------------------
@@ -120,7 +146,7 @@ data RationalNumber = RationalNumber Integer Integer
   deriving Show
 
 instance Eq RationalNumber where
-  p == q = todo
+  (RationalNumber n1 d1) == (RationalNumber n2 d2) = n1 * d2 == d1 * n2
 
 ------------------------------------------------------------------------------
 -- Ex 9: implement the function simplify, which simplifies rational a
@@ -140,7 +166,9 @@ instance Eq RationalNumber where
 -- Hint: Remember the function gcd?
 
 simplify :: RationalNumber -> RationalNumber
-simplify p = todo
+simplify (RationalNumber n d) = RationalNumber (n `div` g) (d `div` g)
+  where
+    g = gcd n d
 
 ------------------------------------------------------------------------------
 -- Ex 10: implement the typeclass Num for RationalNumber. The results
@@ -161,12 +189,14 @@ simplify p = todo
 --   signum (RationalNumber 0 2)             ==> RationalNumber 0 1
 
 instance Num RationalNumber where
-  p + q = todo
-  p * q = todo
-  abs q = todo
-  signum q = todo
-  fromInteger x = todo
-  negate q = todo
+  (RationalNumber n1 d1) + (RationalNumber n2 d2) = 
+    simplify (RationalNumber (n1*d2 + d1*n2) (d1*d2))
+  (RationalNumber n1 d1) * (RationalNumber n2 d2) = 
+    simplify (RationalNumber (n1*n2) (d1*d2))
+  abs (RationalNumber n d) = RationalNumber (abs n) (abs d)
+  signum (RationalNumber n d) = RationalNumber (signum n * signum d) 1
+  fromInteger n = RationalNumber n 1
+  negate (RationalNumber n d) = RationalNumber (0-n) d
 
 ------------------------------------------------------------------------------
 -- Ex 11: a class for adding things. Define a class Addable with a
@@ -180,6 +210,18 @@ instance Num RationalNumber where
 --   add 1 zero             ==>  1
 --   add [1,2] [3,4]        ==>  [1,2,3,4]
 --   add zero [True,False]  ==>  [True,False]
+
+class Addable a where
+  zero :: a
+  add :: a -> a -> a
+
+instance Addable Integer where
+  zero = 0
+  add = (+)
+
+instance Addable [a] where
+  zero = []
+  add = (++)
 
 
 ------------------------------------------------------------------------------
@@ -211,4 +253,22 @@ data Color = Red | Green | Blue
   deriving (Show, Eq)
 data Suit = Club | Spade | Diamond | Heart
   deriving (Show, Eq)
+
+class Cycle a where
+  step :: a -> a
+
+  stepMany :: Int -> a -> a
+  stepMany n x =  iterate step x !! n
+
+
+instance Cycle Color where
+  step Red = Green
+  step Green = Blue
+  step Blue = Red
+
+instance Cycle Suit where
+  step Club = Spade
+  step Spade = Diamond
+  step Diamond = Heart
+  step Heart = Club
 
--- exercises/Set7.hs
+++ exercises/Set7.hs
@@ -26,11 +26,11 @@ data Velocity = Velocity Double
 
 -- velocity computes a velocity given a distance and a time
 velocity :: Distance -> Time -> Velocity
-velocity = todo
+velocity (Distance d) (Time t) = Velocity (d / t)
 
 -- travel computes a distance given a velocity and a time
 travel :: Velocity -> Time -> Distance
-travel = todo
+travel (Velocity v) (Time t) = (Distance (v * t))
 
 ------------------------------------------------------------------------------
 -- Ex 2: let's implement a simple Set datatype. A Set is a list of
@@ -49,15 +49,18 @@ data Set a = Set [a]
 
 -- emptySet is a set with no elements
 emptySet :: Set a
-emptySet = todo
+emptySet = Set []
 
 -- member tests if an element is in a set
 member :: Eq a => a -> Set a -> Bool
-member = todo
+member e (Set s) = e `elem` s
 
 -- add a member to a set
-add :: a -> Set a -> Set a
-add = todo
+add :: Ord a => a -> Set a -> Set a
+add e (Set s) = if e `elem` rest then Set s else Set (first ++ (e:rest))
+  where
+    first = Prelude.takeWhile (< e) s
+    rest = Prelude.dropWhile (< e) s
 
 ------------------------------------------------------------------------------
 -- Ex 3: a state machine for baking a cake. The type Event represents
@@ -92,10 +95,19 @@ add = todo
 data Event = AddEggs | AddFlour | AddSugar | Mix | Bake
   deriving (Eq,Show)
 
-data State = Start | Error | Finished
+data State = Start | EggsAdded | SugarAdded | FlourAdded | AllIngredients | Mixed | Error | Finished
   deriving (Eq,Show)
 
-step = todo
+step :: State -> Event -> State
+step Start AddEggs = EggsAdded
+step EggsAdded AddFlour = FlourAdded
+step EggsAdded AddSugar = SugarAdded
+step FlourAdded AddSugar = AllIngredients
+step SugarAdded AddFlour = AllIngredients
+step AllIngredients Mix = Mixed
+step Mixed Bake = Finished
+step Finished  _ = Finished
+step _ _ = Error
 
 -- do not edit this
 bake :: [Event] -> State
@@ -113,13 +125,18 @@ bake events = go Start events
 --   average (1.0 :| [2.0,3.0])  ==>  2.0
 
 average :: Fractional a => NonEmpty a -> a
-average = todo
+average (n :| []) = n
+average xs = (Prelude.sum xs) / fromIntegral (Prelude.length xs)
+
 
 ------------------------------------------------------------------------------
 -- Ex 5: reverse a NonEmpty list.
 
 reverseNonEmpty :: NonEmpty a -> NonEmpty a
-reverseNonEmpty = todo
+reverseNonEmpty (x :| []) = (x :| [])
+reverseNonEmpty (x :| xs) = (y :| ys)
+  where
+    (y : ys) = Prelude.reverse xs ++ [x]
 
 ------------------------------------------------------------------------------
 -- Ex 6: implement Semigroup instances for the Distance, Time and
@@ -131,6 +148,14 @@ reverseNonEmpty = todo
 -- velocity (Distance 50 <> Distance 10) (Time 1 <> Time 2)
 --    ==> Velocity 20
 
+instance Semigroup Distance where
+  Distance d1 <> Distance d2 = Distance (d1 + d2)
+
+instance Semigroup Velocity where
+  Velocity v1 <> Velocity v2 = Velocity (v1 + v2)
+
+instance Semigroup Time where
+  Time t1 <> Time t2 = Time (t1 + t2)
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement a Monoid instance for the Set type from exercise 2.
@@ -140,6 +165,11 @@ reverseNonEmpty = todo
 --
 -- What are the class constraints for the instances?
 
+instance Ord a => Semigroup (Set a) where
+  (Set xs) <> s = foldr add s xs
+
+instance Ord a => Monoid (Set a) where
+  mempty = emptySet
 
 ------------------------------------------------------------------------------
 -- Ex 8: below you'll find two different ways of representing
@@ -162,28 +192,41 @@ reverseNonEmpty = todo
 
 data Operation1 = Add1 Int Int
                 | Subtract1 Int Int
+                | Multiply1 Int Int
   deriving Show
 
 compute1 :: Operation1 -> Int
 compute1 (Add1 i j) = i+j
 compute1 (Subtract1 i j) = i-j
+compute1 (Multiply1 i j) = i*j
 
 show1 :: Operation1 -> String
-show1 = todo
+show1 (Add1 i j) = show i ++ "+" ++ show j
+show1 (Subtract1 i j) = show i ++ "-" ++ show j
+show1 (Multiply1 i j) = show i ++ "*" ++ show j
 
 data Add2 = Add2 Int Int
   deriving Show
 data Subtract2 = Subtract2 Int Int
   deriving Show
+data Multiply2 = Multiply2 Int Int
+  deriving Show
 
 class Operation2 op where
   compute2 :: op -> Int
+  show2 :: op -> String 
 
 instance Operation2 Add2 where
   compute2 (Add2 i j) = i+j
+  show2 (Add2 i j) = show i ++ "+" ++ show j
 
 instance Operation2 Subtract2 where
   compute2 (Subtract2 i j) = i-j
+  show2 (Subtract2 i j) = show i ++ "-" ++ show j
+
+instance Operation2 Multiply2 where
+  compute2 (Multiply2 i j) = i*j
+  show2 (Multiply2 i j) = show i ++ "*" ++ show j
 
 
 ------------------------------------------------------------------------------
@@ -213,7 +256,14 @@ data PasswordRequirement =
   deriving Show
 
 passwordAllowed :: String -> PasswordRequirement -> Bool
-passwordAllowed = todo
+passwordAllowed pwd req =
+  case req of
+    MinimumLength n -> Prelude.length pwd >= n
+    ContainsSome chars-> not . null $ Data.List.intersect pwd chars
+    DoesNotContain chars-> null (Data.List.intersect pwd chars)
+    And req1 req2-> passwordAllowed pwd req1 && passwordAllowed pwd req2
+    Or req1 req2-> passwordAllowed pwd req1 || passwordAllowed pwd req2
+
 
 ------------------------------------------------------------------------------
 -- Ex 10: a DSL for simple arithmetic expressions with addition and
@@ -235,17 +285,31 @@ passwordAllowed = todo
 --     ==> "(3*(1+1))"
 --
 
-data Arithmetic = Todo
+data Arithmetic = 
+    Literal Int 
+  | Add Arithmetic Arithmetic 
+  | Multiply Arithmetic Arithmetic
   deriving Show
 
 literal :: Integer -> Arithmetic
-literal = todo
+literal n = Literal (fromIntegral n)
 
 operation :: String -> Arithmetic -> Arithmetic -> Arithmetic
-operation = todo
+operation op arg1 arg2 = 
+  case op of 
+    "+" -> Add arg1 arg2
+    "*" -> Multiply arg1 arg2
 
 evaluate :: Arithmetic -> Integer
-evaluate = todo
+evaluate expr = 
+  case expr of
+    Literal n -> fromIntegral n
+    Add expr1 expr2 -> evaluate expr1 + evaluate expr2
+    Multiply expr1 expr2 -> evaluate expr1 * evaluate expr2
 
 render :: Arithmetic -> String
-render = todo
+render expr = 
+  case expr of
+    Literal n -> show n
+    Add expr1 expr2 -> "(" ++ render expr1 ++ "+" ++ render expr2 ++ ")"
+    Multiply expr1 expr2 -> "(" ++ render expr1 ++ "*" ++ render expr2 ++ ")"
